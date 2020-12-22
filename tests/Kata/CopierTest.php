4be357aed7be71c905e0dc9c7306cea340c0e19a
danielescillia <daniele.scillia@gmail.com>
2020-12-22 11:30:02 +0100

    added another test

--- tests/Kata/CopierTest.php
+++ tests/Kata/CopierTest.php
@@ -22,7 +22,7 @@ class CopierTest extends TestCase
         $this->copier = new Copier($this->source, $this->destination);
     }
 
-    public function testHandleReturnTrue(): void
+    public function testStringAbc(): void
     {
         $this->source
         ->expects($this->exactly(4))
@@ -36,6 +36,36 @@ class CopierTest extends TestCase
              $this->equalTo('a'),
              $this->equalTo('b'),
              $this->equalTo('c'),
+        ));
+
+        $this->copier->copy();
+    }
+
+    public function testStringDanielescillia(): void
+    {
+        $this->source
+        ->expects($this->exactly(15))
+        ->method('getChar')
+        ->will($this->onConsecutiveCalls('d', 'a', 'n', 'i', 'e', 'l', 'e', 's', 'c', 'i', 'l', 'l', 'i', 'a', '\n'));
+
+        $this->destination
+        ->expects($this->exactly(14))
+        ->method('setChar')
+        ->with($this->logicalOr(
+             $this->equalTo('d'),
+             $this->equalTo('a'),
+             $this->equalTo('n'),
+             $this->equalTo('i'),
+             $this->equalTo('e'),
+             $this->equalTo('l'),
+             $this->equalTo('e'),
+             $this->equalTo('s'),
+             $this->equalTo('c'),
+             $this->equalTo('i'),
+             $this->equalTo('l'),
+             $this->equalTo('l'),
+             $this->equalTo('i'),
+             $this->equalTo('a'),
         ));
 
         $this->copier->copy();
